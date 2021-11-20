791136b3eb5bddc1fc154754f50c1e2e9ce5c66f
ikael <ikael@student.21-school.ru>
2021-11-20 18:35:21 +0300

    Request.hpp Server.hpp Responce.hpp
    small fixes

--- src/responses/Response.cpp
+++ src/responses/Response.cpp
@@ -4,11 +4,13 @@
 Response :: Response(Request &request){
 	t_fileInfo file;
 	if (urlInfo(request.getUrl(), &file))
+	{
 		_statusCode = 200;
+		_bodySize = file.fLength;
+		_contentType = file.fExtension;
+	}
 	else
 		_statusCode = 404;
-	_bodySize = file.fLength;
-	_contentType = file.fExtension;
 	_url = request.getUrl();
 	_leftBytes = 1;
 	_inProc = false;
@@ -23,13 +25,16 @@ string Response :: makeStatusLine(){
 }
 
 std::string Response :: makeHeaders(){
-	const std::time_t current_time = std::time(0);	
+	const std::time_t current_time = std::time(0);
 	_tmpHead += "Server: ~Server()" + string(CRLF);
 	_tmpHead += "Date: " + string(ctime(&current_time));
-	_tmpHead += "Content-Type: " + _contentType + string(CRLF);
-	_tmpHead += "Content-Length: " + ft_itoa(_bodySize) + string(CRLF);
+	if (_statusCode == 200)
+	{
+		_tmpHead += "Content-Type: " + _contentType + string(CRLF);
+		_tmpHead += "Content-Length: " + ft_itoa(_bodySize) + string(CRLF);
+		_tmpHead += "Accept-Ranges: bytes" + string(CRLF);
+	}
 	_tmpHead += "Connection: close" + string(CRLF);
-	_tmpHead += "Accept-Ranges: bytes" + string(CRLF);
 	return(_tmpHead);
 }
 
@@ -41,8 +46,8 @@ std::string Response :: makeBody(){
 	if (!FILE.is_open())
 		throw ErrorException("ERROR OPENNING URL");
 	while (getline(FILE, line))
-			_body += line;
-	_body += string(CRLF);
+			_body += line += "\n";
+	_body += string(CRLF) + string(CRLF);
 	return (_body);
 }
 
@@ -61,7 +66,7 @@ void Response :: sendRes(int socket){
 	res = send(socket, _response.c_str(), _response.length(), 0);
 	if (res == -1)
 		throw ErrorException("ERROR SENDING DATA");
-	std::cerr << "LEFT AFTER SEND\n" << _response << std::endl; 
+	std::cerr << "LEFT AFTER SEND\n" << _response << std::endl;
 	_response = _response.substr(res);
 	_leftBytes -= res;
 	if (_leftBytes < 1)
